2c4e61fb78dd2e5c9e2d1fc9656c58f0de3fa8f1
delia-pixel <tpatriciareinedia@gmail.com>
2022-11-24 05:53:21 +0100

    start

---
+++ myFlightApp/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+
+const routes: Routes = [
+  { path: 'search', component:SearchFormComponent}
+];
+  
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
