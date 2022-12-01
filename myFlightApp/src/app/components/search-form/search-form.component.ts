9612d2c4adf5ab10802e24e705638aaeec677ffb
delia-pixel <tpatriciareinedia@gmail.com>
2022-12-01 19:26:31 +0100

    mat angular

--- myFlightApp/src/app/components/search-form/search-form.component.ts
+++ myFlightApp/src/app/components/search-form/search-form.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NgxMaterialTimepickerTheme } from 'ngx-material-timepicker';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
 
-  constructor() { }
+  searchFlightForm!: FormGroup;
+
+  timeTheme: NgxMaterialTimepickerTheme = {
+    container: {
+      bodyBackgroundColor: 'salmon',
+      buttonColor: 'sandybrown',
+    },
+    dial: {
+      dialBackgroundColor: 'sandybrown',
+      dialEditableBackgroundColor: '#eee',
+    },
+    clockFace: {
+      // clockFaceBackgroundColor: 'red',
+      clockHandColor: '#ddd',
+      clockFaceInnerTimeInactiveColor: '#000',
+    },
+  };
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.searchFlightForm = new FormGroup({
+      company: new FormControl(''),
+      trip: new FormControl(''),
+      flight: new FormControl(''),
+      aircraft: new FormControl(''),
+      departureFrom: new FormControl(''),
+      arrivingAt: new FormControl(''),
+      deparMax: new FormControl(''),
+      deparMin: new FormControl(''),
+      departureTimeMin: new FormControl(''),
+      departureTimeMax: new FormControl(''),
+      arrivingDateMin: new FormControl('') ,
+      arrivingDateMax: new FormControl(''),
+      arrivingTimeMin: new FormControl(''),
+      arrivingTimeMax: new FormControl(''),
+      flightMin: new FormControl(''),
+      flightMax: new FormControl(''),
+      fareMin: new FormControl(''),
+      fareMax: new FormControl(''),
+      backDateMin: new FormControl(''),
+      backDateMax: new FormControl(''),
+      backTimeMin: new FormControl(''),
+      backTimeMax: new FormControl(''),
+      connectionMin: new FormControl(''),
+      connectionMax: new FormControl(''),
+
+    });
+
   }
 
+  resetForm(form: FormGroup) {
+    form.reset();
+  }
 }
--- myFlightApp/src/app/shared/shared.module.ts
+++ myFlightApp/src/app/shared/shared.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFormComponent } from '../components/search-form/search-form.component';
-
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    SearchFormComponent
-  ],
+  declarations: [SearchFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaterialTimepickerModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  exports: [
-    SearchFormComponent
-  ]
+  exports: [SearchFormComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
